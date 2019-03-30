40b21ad0364d55820ddcc6a3ec742e3d464e74bf
dlichota <dlichota@gmail.com>
2019-03-30 08:30:25 -0400

    padding 50%

--- js/iframe.js
+++ js/iframe.js
@@ -14,7 +14,7 @@ if (v.length > 0) {
     s.innerHTML = '.codegena_iframe{overflow:hidden;position:relative;cursor:hand;cursor:pointer;}' +
         '.codegena_iframe .thumb{bottom:0;display:block;left:0;margin:auto;max-width:100%;position:absolute;right:0;top:0;width:100%;height:auto;-webkit-filter: brightness(.8);}' +
         '.codegena_iframe .thumb:hover{transform: scale(1.3);transition: all 1s cubic-bezier(0.71, 0.15, 0.37, 0.82);}' +
-        '.responsive_iframe{position:relative;padding-bottom:10%;padding-top:30px;height:0;-webkit-overflow-scrolling:touch;}.responsive_iframe iframe{position:absolute;top:0;left:0;width:100%;height:50%;}';
+        '.responsive_iframe{position:relative;padding-bottom:50%;padding-top:30px;height:0;-webkit-overflow-scrolling:touch;}.responsive_iframe iframe{position:absolute;top:0;left:0;width:100%;height:50%;}';
     document.body.appendChild(s);
 }
 
